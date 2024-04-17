f4881e208ef2e4f341dc0ec74831dc9dea50c305
MargaritaSukhova <margarytasu@gmail.com>
2024-04-18 02:00:56 +0300

    added validation on create user

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,26 @@
 import { Controller, Post, Body, Get } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User as UserModel } from '@prisma/client';
+import { IsNotEmpty, IsEmail } from 'class-validator';
+
+class CreateUserDto {
+  @IsNotEmpty()
+  name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  message: string;
+}
 
 @Controller()
 export class AppController {
   constructor(private readonly userService: UserService) {}
 
   @Post('user')
-  async signupUser(
-    @Body() userData: { name: string; email: string; message: string },
-  ): Promise<UserModel> {
+  async signupUser(@Body() userData: CreateUserDto): Promise<UserModel> {
     return this.userService.createUser(userData);
   }
 
